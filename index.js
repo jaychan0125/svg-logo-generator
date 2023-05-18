64de9dc06666ca471a0db5b3403fe3641fff8237
jaychan0125 <joycechan0125@gmail.com>
2023-05-18 00:17:12 -0400

    finished project, adding in new README generated from previous challenge

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require("fs");
+const { generateLogo } = require('./lib/shapes.js')
 
-const questions = inquirer 
+const questions = inquirer
     .prompt([
         {
             type: "input",
             message: "Please enter up to 3 characters for your logo.",
-            name: "char", 
+            name: "text",
             validate: (char) => {
                 if (char.length > 3) {
                     return 'Please enter only up to 3 characeters.'
@@ -15,29 +16,41 @@ const questions = inquirer
             }
         },
         {
-            type: "input", 
+            type: "input",
             message: "What colour would you like the text (keyword or hexadecimal number)?",
             name: "textColour"
-        }, 
+        },
         {
-            type: "list", 
-            message: "What shape would you like the logo to be?", 
-            name: "shape", 
-            choices: ["circle", "triangle", "square"]
-        }, 
+            type: "list",
+            message: "What shape would you like the logo to be?",
+            name: "shape",
+            choices: ["triangle", "circle", "square"]
+        },
         {
-            type: "input", 
-            message: "What background colour would you like the shape to be (keyword or hexadecimal number)?", 
+            type: "input",
+            message: "What background colour would you like the shape to be (keyword or hexadecimal number)?",
             name: "shapeColour"
         }
     ])
-    .then((answers) => {
-        // Use user feedback for... whatever!!
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
-        } else {
-          // Something else went wrong
-        }
+
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}`, data, (err) => {
+        err ? console.error(err) : console.log(`Generated ${fileName}`)
+    });
+}
+
+questions
+    .then((response) => {
+        // const { text, textColour, shape, shapeColour } = response;
+        return generateLogo(response);
+    })
+    .then((logoData) => {
+        writeToFile('logo.svg', logoData);
+    })
+    .catch((err) => {
+        console.error(err)
       });
+
+
+
+
